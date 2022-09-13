c3a1c0712db25ae80aed958bd03ff436da0440b6
MICAH ADDAH <addahmicah@gmail.com>
2022-09-13 16:36:10 -0700

    class square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define class Square with private attribute size and validates size"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """
+        Initialize a new square
+
+        Args:
+            __size (int): size of a side of square, defaults to 0 if none
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
