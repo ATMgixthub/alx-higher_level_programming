f56a9c36638f8a225993864bd2eb7fd84cd75ce0
ATMgixthub <105108630+ATMgixthub@users.noreply.github.com>
2022-12-09 11:28:32 +0000

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """
-return states starting with 'N'
-parameters given to script: username, password, database
+Lists all states with a name starting with N
 """
-
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
 
-    # create cursor to exec queries using SQL; filter names starting with 'N'
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    for row in cursor.fetchall():
-        if row[1][0] == 'N':
-            print(row)
-    cursor.close()
-    db.close()
+    for state in states:
+        print(state)
